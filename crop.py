886fc226cef4044798fb1537f4bf6715ef59663b
Petr Chlup <chlupp@natur.cuni.cz>
2023-04-24 16:50:36 +0200

    Attempt to fix error on OCR manual seconds reading. The cause is that the root window is closed when calling a method on it.

--- crop.py
+++ crop.py
@@ -351,7 +351,7 @@ def process_OCR_text(detected_text, frame):
         dialog_width = img_frame_width
         dialog_height = dialog.winfo_reqheight()
         dialog_pos_x = int((screen_width // 2) - (img_frame_width // 2))
-        dialog_pos_y = max((img_frame_pos_y + img_frame_height),(screen_height - (img_frame_height//2)))
+        dialog_pos_y = min((img_frame_pos_y + img_frame_height),(screen_height - (img_frame_height//2)))
         dialog.geometry(f"{dialog_width}x{dialog_height}+{dialog_pos_x}+{dialog_pos_y}")
 
 
