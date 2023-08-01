b39b99676f2823007d80cabf29e502b6793f6cdc
Petr <petaschlup@seznam.cz>
2023-08-01 10:55:23 +0200

    New version of requirements.txt and new installation script

---
+++ install_packages.py
@@ -0,0 +1,14 @@
+import subprocess
+
+def install_packages(requirements_file):
+    with open(requirements_file) as f:
+        for line in f:
+            package = line.strip()
+            try:
+                subprocess.check_call(["pip", "install", package])
+            except subprocess.CalledProcessError as e:
+                print(f"Error installing {package}: {e}")
+
+if __name__ == "__main__":
+    requirements_file = "requirements.txt"
+    install_packages(requirements_file)
