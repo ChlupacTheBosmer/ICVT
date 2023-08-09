5304db35c7019225d58344141378af18bd9e9f9d
Petr Chlup <chlupp@natur.cuni.cz>
2023-08-09 17:02:35 +0200

    Working on install_packages.py

--- install_packages.py
+++ install_packages.py
@@ -1,14 +1,17 @@
 import subprocess
+import re
 
 def install_packages(requirements_file):
-    with open(requirements_file, encoding='utf-8-sig') as f:
+    with open(requirements_file) as f:
         for line in f:
             package = line.strip()
+            package_name = re.match(r'^[a-zA-Z0-9_-]+', package).group()
             try:
                 subprocess.check_call(["pip", "install", package])
+                print(package_name)
             except subprocess.CalledProcessError as e:
                 print(f"Error installing {package}: {e}")
 
 if __name__ == "__main__":
-    requirements_file = "requirements.txt"
+    requirements_file = "requirements_linux.txt"
     install_packages(requirements_file)
