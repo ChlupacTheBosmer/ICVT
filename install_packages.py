5f94f7b398fcfd30c9c49acc44a471655cbed18d
Petr <petaschlup@seznam.cz>
2023-08-21 19:45:21 +0200

    Added script for installing singularity packages. construct_singularity.py

--- install_packages.py
+++ install_packages.py
@@ -11,8 +11,8 @@ def install_packages(requirements_file, bare_installation, installer):
             package = line.strip()
             package_name = re.match(r'^[a-zA-Z0-9_-]+', package).group()
             try:
-                subprocess.check_call(["pip", "install", package])
-                #print(package_name)
+                subprocess.check_call([installer, "install", package])
+                #print(f"{installer} install {package}")
                 success_list.append(f"{package_name}")
             except subprocess.CalledProcessError as e:
                 print(f"Error installing {package}: {e}")
