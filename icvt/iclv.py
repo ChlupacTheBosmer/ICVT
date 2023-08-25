469311bde10b2d5483513d9aff462600e80c8d97
Petr <petaschlup@seznam.cz>
2023-08-25 20:32:18 +0200

    Generator focus rework

--- icvt/iclv.py
+++ icvt/iclv.py
@@ -1,13 +1,19 @@
 # ICVT modules
 from modules.labels.image_viewer import ImageViewer
 
+# Default python packages
+import sys
+import os
+
+import PyQt5
+
+pyqt = os.path.dirname(PyQt5.__file__)
+os.environ['QT_PLUGIN_PATH'] = os.path.join(pyqt, "Qt5/plugins")
+
 # Extra packages
 from tkinter import filedialog
 from PyQt5.QtWidgets import QApplication
 
-# Default python packages
-import sys
-import os
 
 if __name__ == '__main__':
     # Specify the folder path
